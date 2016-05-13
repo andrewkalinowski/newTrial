5d9e9c66316b36b3c63fc944416c1825fe707d18
DragonmanJoel <DragonmanJoel@joels-air.home>
2016-05-13 14:12:38 -0700

    5/13/2016 - Initial commit to repository

---
+++ dragonKnight/src/byui/cit260/dragonknight/model/Item.java
@@ -0,0 +1,84 @@
+package byui.cit260.dragonknight.model;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+/**
+ *
+ * @author DragonmanJoel
+ */
+public class Item implements Serializable{
+    
+    // class instance variables
+    private String name;
+    private String description;
+    private String cost;
+
+    public Item() {
+    }
+    
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getCost() {
+        return cost;
+    }
+
+    public void setCost(String cost) {
+        this.cost = cost;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 37 * hash + Objects.hashCode(this.name);
+        hash = 37 * hash + Objects.hashCode(this.description);
+        hash = 37 * hash + Objects.hashCode(this.cost);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Item other = (Item) obj;
+        if (!Objects.equals(this.name, other.name)) {
+            return false;
+        }
+        if (!Objects.equals(this.description, other.description)) {
+            return false;
+        }
+        if (!Objects.equals(this.cost, other.cost)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Item{" + "name=" + name + ", description=" + description + ", cost=" + cost + '}';
+    }
+    
+    
+    
+}
